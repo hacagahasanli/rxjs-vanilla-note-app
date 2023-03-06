c74be74383136fecc941dc6e7898e2009abe60be
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-03-06 22:26:33 +0400

    Unnecassary styles

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
-import { fromEvent, throttleTime, scan } from 'rxjs';
+import { fromEvent, throttleTime, scan, tap, toArray, map, filter } from 'rxjs';
 import { v4 } from 'uuid';
 
 const note = document.getElementById("note")
 const btn = document.getElementById("button")
 const lists = document.getElementById("lists")
 
+
 const addNote = fromEvent(btn, "click")
   .pipe(
     throttleTime(1000),
-    scan((notes) => [...notes, note.value], [note.value])
+    scan((notes) => [...notes, note.value], []),
+    map(arr => arr.sort((a, b) => a.charAt(0).localeCompare(b.charAt(0))))
   )
 
 const subscription = addNote.subscribe((notes) => {
+  console.log(notes, "NOTES")
   if (note.value) {
     const li = document.createElement("li");
     li.innerText = notes[notes.length - 1];
     li.setAttribute("id", v4())
     lists.appendChild(li)
-    return li;
+    return lists;
   }
   subscription?.unsubscribe();
 })
-
-import { of, map } from 'rxjs';
-
-of(1, 2, 3)
-  .pipe(map((x) => x * x))
-  .subscribe((v) => console.log(`value: ${v}`));
