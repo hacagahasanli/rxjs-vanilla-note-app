29361d3b75c774242363cecc44dcf0882d6f04d6
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-03-06 22:55:59 +0400

    Sort Added Note alphabetically

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-import { fromEvent, throttleTime, scan, tap, toArray, map, filter } from 'rxjs';
+import { fromEvent, throttleTime, scan, tap, toArray, map, filter, from } from 'rxjs';
 import { v4 } from 'uuid';
 
 const note = document.getElementById("note")
 const btn = document.getElementById("button")
 const lists = document.getElementById("lists")
 
+const createList = (note) => {
+  const li = document.createElement("li");
+  li.innerText = note;
+  li.setAttribute("id", v4())
+  return li;
+}
 
 const addNote = fromEvent(btn, "click")
   .pipe(
     throttleTime(1000),
-    scan((notes) => [...notes, note.value], []),
-    map(arr => arr.sort((a, b) => a.charAt(0).localeCompare(b.charAt(0))))
+    scan((notes) => {
+      const arrayofNotes = [...notes, note.value]
+      const sortedArray = arrayofNotes.sort((a, b) => a.charAt(0).localeCompare(b.charAt(0)))
+      return sortedArray
+    }, [])
   )
 
 const subscription = addNote.subscribe((notes) => {
-  console.log(notes, "NOTES")
-  if (note.value) {
-    const li = document.createElement("li");
-    li.innerText = notes[notes.length - 1];
-    li.setAttribute("id", v4())
-    lists.appendChild(li)
-    return lists;
-  }
-  subscription?.unsubscribe();
+  while (lists.firstChild) lists.removeChild(lists.firstChild);
+  notes.map((note) => lists.appendChild(createList(note)))
 })
